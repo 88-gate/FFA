ec1fc37b51e97b5def38e5757b3c22d454a455d3
kaitp1016 <ikakun31016@gmail.com>
2026-03-13 23:54:26 +0900

    復讐をしたときに音を鳴らすように

--- src/main/kotlin/me/kaitp1016/ffa/game/Revenge.kt
+++ src/main/kotlin/me/kaitp1016/ffa/game/Revenge.kt
@@ -7,11 +7,13 @@ import me.kaitp1016.ffa.utils.NMSUtils.asCraftPlayer
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
 import org.bukkit.Bukkit
+import org.bukkit.Sound
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntityDamageEvent
 import org.bukkit.event.entity.PlayerDeathEvent
+import kotlin.random.Random
 
 object Revenge: Listener {
     data class RevengeTarget(val player: Player, val killer: Player, var tick: Int)
@@ -31,14 +33,14 @@ object Revenge: Listener {
                 killer.inventory.setItem(slot,item)
             }
 
-            player.inventory.clear()
             event.drops.clear()
+            killer.world.playSound(killer.location,Sound.ENTITY_VINDICATOR_CELEBRATE,2f,1.15f)
 
             val reward = Settings.REVENGE_REWARD.getValue()
-            player.asCraftPlayer().handle.addMoney(reward)
-            player.sendMessage("§a復讐を成功して §e${reward}コイン §aを受け取りました!")
+            killer.asCraftPlayer().handle.addMoney(reward)
+            killer.sendMessage("§a復讐を成功して §e${reward}コイン §aを受け取りました!")
 
-            val text = Component.text("").color(NamedTextColor.YELLOW).append(killer.name().append(Component.text(" は ").color(NamedTextColor.YELLOW).append(player.name()).append(Component.text(" に復讐を果たした!").color(NamedTextColor.YELLOW))))
+            val text = Component.empty().color(NamedTextColor.YELLOW).append(killer.name().append(Component.text(" は ").color(NamedTextColor.YELLOW).append(player.name()).append(Component.text(" に復讐を果たした!").color(NamedTextColor.YELLOW))))
             Bukkit.getOnlinePlayers().forEach {
                 it.sendMessage(text)
             }
@@ -48,8 +50,8 @@ object Revenge: Listener {
 
         revenges.add(RevengeTarget(player,killer, Settings.REVENGE_EXPIRE_TIME.getValue()))
 
-        player.sendMessage(Component.text("あなたは ").color(NamedTextColor.YELLOW).append(player.name().append(Component.text(" §rに復讐したいと感じた...").color(NamedTextColor.YELLOW))))
-        killer.sendMessage(Component.text("あなたは ").color(NamedTextColor.YELLOW) .append(player.name().append(Component.text(" §rから復讐したいという意思を感じ取った...").color(NamedTextColor.YELLOW))))
+        player.sendMessage(Component.text("あなたは ").color(NamedTextColor.YELLOW).append(killer.name().append(Component.text(" §rに復讐したいと感じた...").color(NamedTextColor.YELLOW))))
+        killer.sendMessage(Component.text("あなたは ").color(NamedTextColor.YELLOW).append(player.name().append(Component.text(" §rから復讐したいという意思を感じ取った...").color(NamedTextColor.YELLOW))))
     }
 
     @EventHandler
@@ -60,6 +62,7 @@ object Revenge: Listener {
 
         if (revenges.any { it.player.uniqueId == damager.uniqueId && it.killer.uniqueId == player.uniqueId }) {
             event.damage *= Settings.REVENGE_DAMAGE_MULTIPLIER.getValue()
+            player.world.playSound(damager.location, Sound.ENTITY_RAVAGER_HURT,2f, 0.89f)
         }
     }
 
